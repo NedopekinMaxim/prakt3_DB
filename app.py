d9352b7cabc35feafda455c5397c3553e117eaf8
Maxim <90789247+NedopekinMaxim@users.noreply.github.com>
2021-12-24 18:51:35 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -2,45 +2,45 @@ import requests
 from flask import Flask, render_template, request, redirect
 import psycopg2
 
-app = Flask(__name__)   # создаем приложение
+app = Flask(__name__)
 
-conn = psycopg2.connect(database="service_db",  # подключение к бд
+conn = psycopg2.connect(database="service_db",
                         user="postgres",
                         password="",
                         host="localhost",
                         port="5432")
 
-cursor = conn.cursor()  # добавляем курсор
+cursor = conn.cursor()
 
-@app.route('/login/', methods=['POST', 'GET'])  # декоратор
+@app.route('/login/', methods=['POST', 'GET'])
 def login():
 
-    if request.method == 'POST':    # исключение на проверку пост
+    if request.method == 'POST':
         if request.form.get("login"):
             username = request.form.get('username')
             password = request.form.get('password')
             cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
             records = list(cursor.fetchall())
 
-            if username and password and records:   # исключение на пустой ввод
-                #рендер страницы аккаунта
+            if username and password and records:
+
                 return render_template('account.html', full_name=records[0][1], log=username, pas=password)
         elif request.form.get("registration"):
-            return redirect("/registration/")     # перенаправление на регистрацию
-    return render_template('login.html')    # рендер страницы регистрация
+            return redirect("/registration/")
+    return render_template('login.html')
 
 
-@app.route('/registration/', methods=['POST', 'GET'])   # декоратор
+@app.route('/registration/', methods=['POST', 'GET'])
 def registration():
 
-    if request.method == 'POST':    # проверка на пост
+    if request.method == 'POST':
         name = request.form.get('name')
         login = request.form.get('login')
         password = request.form.get('password')
 
-        if str(name) and str(login) and str(password):  # проверка на исключения
+        if str(name) and str(login) and str(password):
 
-            cursor.execute("SELECT * FROM service.users WHERE login=%s", (str(login),)) #обращение к бд
+            cursor.execute("SELECT * FROM service.users WHERE login=%s", (str(login),))
             records = cursor.fetchall()
             if records:
                 return redirect("/registration/")
@@ -48,6 +48,6 @@ def registration():
                 cursor.execute('INSERT INTO service.users (full_name, login, password) VALUES (%s, %s, %s);', (str(name), str(login), str(password)))
                 return redirect('/login/')
 
-        conn.commit()   # внесение данных в бд
+        conn.commit()
 
-    return render_template('registration.html') # рендер регистрации
+    return render_template('registration.html')
